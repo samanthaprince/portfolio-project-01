f04ddcd0d295adcda32c62a0d5a570773e10b52f
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-01-07 20:43:20 -0800

    Initial blog articles file with some text in it

---
+++ scripts/blogArticles.js
@@ -0,0 +1,18 @@
+var rawData = [
+  {
+    title:      'Day One, Assignment One',
+    category:   'Career Change 101',
+    author:     'Samantha Prince',
+    authorUrl:   '',
+    publishedOn: '2016-01-04',
+    body:         '<p>The first day of class was hard and I felt like I was drowning before i even started.</p>'
+  },
+  {
+    title:      'Maybe things are looking Up?',
+    category:   'Career Change 101',
+    author:     'Samantha Prince',
+    authorUrl:   '',
+    publishedOn: '2016-01-06',
+    body:         '<p>The second day of class started to get better, until we used Git/Github.</p>'
+  },
+];
