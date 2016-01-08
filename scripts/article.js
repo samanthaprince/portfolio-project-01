0964934695c9aa1e9f5bc168cb7bd99d34fe09f4
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-01-07 20:47:07 -0800

    Added article function

---
+++ scripts/article.js
@@ -0,0 +1,19 @@
+var articles = [];
+
+function Article(opts) {
+  this.title = opts.title;
+  this.category = opts.category;
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
+}
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+
+  $newArticle.find('h1').html(this.title);
+
+
+  return $newArticle;
+};
