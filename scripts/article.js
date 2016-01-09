378c00b51de86019f55ff0945a78fa25f5f82af9
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-01-09 09:45:21 -0800

    add more populating code

--- scripts/article.js
+++ scripts/article.js
@@ -13,7 +13,13 @@ Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
 
   $newArticle.find('h1').html(this.title);
+  $newArticle.find('.byline a').html(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('article-body').html(this.body);
 
+  $newArticle.find('time[pubdate]').attr('title',this.publishedOn);
+
+  $newArticle.find('time').html('about' + parseInt(new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
 
   return $newArticle;
 };
